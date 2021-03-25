a0d69d57e44e862f9230615ffbcca28d17f07b67
ParkJunHa <jh01love@naver.com>
2021-03-25 20:44:15 +0900

    test
    test

--- betaSandol/lambda_function.py
+++ betaSandol/lambda_function.py
@@ -3,9 +3,9 @@ import lambda_module as Module
 
 
 def lambda_handler(event, context):
-    request_body = json.loads(event['body'])
-    param = request_body['action']['params']
-    key = list(param.keys())  # 입력으로 들어오는 값을 여기서 처리함
+    # request_body = json.loads(event['body'])
+    # param = request_body['action']['params']
+    # key = list(param.keys())  # 입력으로 들어오는 값을 여기서 처리함
     # 여러개 들어오는 경우 필수 파라미터 명이 key[0]에 들어감
     result = {
         "version": "2.0",
