ddc5bc094f6a82c824057b57413e6d46fcb83386
Cycrypto <jh01love@naver.com>
2021-03-28 19:09:32 +0900

    smaller test

--- betaSandol/lambda_prototype.py
+++ betaSandol/lambda_prototype.py
@@ -8,10 +8,18 @@ def lambda_handler(event, context):
         param = request_body['action']['params']
         key = list(param.keys())  # 입력으로 들어오는 값을 여기서 처리함
         # 여러개 들어오는 경우 필수 파라미터 명이 key[0]에 들어감
-    except Exception as e:
         return {
             'statusCode': 200,
-            'body': json.dumps("error"+str(e)),
+            'body': json.dumps(str(key)),
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+    except Exception as e:
+        result = "error"+str(e)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(result),
             'headers': {
                 'Access-Control-Allow-Origin': '*',
             }
