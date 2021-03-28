aa0b8a7e75e178d824e7a1d4dfb894e549d90ee2
Cycrypto <jh01love@naver.com>
2021-03-28 21:39:17 +0900

    return json string

--- betaSandol/lambda_prototype.py
+++ betaSandol/lambda_prototype.py
@@ -1,20 +1,20 @@
 import json
 import lambda_prototype_module as Module
-
+import base64
 
 def lambda_handler(event, context):
     try:
-        # request_body = json.loads(event['body'])
-        # param = request_body['action']['params']
-        # key = list(param.keys())  # 입력으로 들어오는 값을 여기서 처리함
-        # # 여러개 들어오는 경우 필수 파라미터 명이 key[0]에 들어감
+        request_body = json.loads(event['body'])
+        param = request_body['action']['params']
+        key = list(param.keys())  # 입력으로 들어오는 값을 여기서 처리함
+        # 여러개 들어오는 경우 필수 파라미터 명이 key[0]에 들어감
         result = {
             "version": "2.0",
             "template": {
                 "outputs": [
                     {
                         "simpleText": {
-                            "text": str(event)
+                            "text": str(request_body)
                         }
                     }
                 ]
