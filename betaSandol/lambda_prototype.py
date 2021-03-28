9701b9c8ad38edf63407b3d6101f63dd3f821a5c
Cycrypto <jh01love@naver.com>
2021-03-28 19:20:19 +0900

    .

--- betaSandol/lambda_prototype.py
+++ betaSandol/lambda_prototype.py
@@ -8,20 +8,37 @@ def lambda_handler(event, context):
         param = request_body['action']['params']
         key = list(param.keys())  # 입력으로 들어오는 값을 여기서 처리함
         # 여러개 들어오는 경우 필수 파라미터 명이 key[0]에 들어감
-        return {
-            'statusCode': 200,
-            'body': json.dumps(str(key)),
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-            }
-        }
-    except Exception as e:
-        result = "error"+str(e)
-        return {
-            'statusCode': 200,
-            'body': json.dumps(result),
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
+        result = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": str(key)
+                        }
+                    }
+                ]
             }
         }
 
+    except Exception as e:
+        result = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": str(e)
+                        }
+                    }
+                ]
+            }
+        }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(result),
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+        }
+    }
