428bb752049595ca8a7763a5f2f7a29002d0975f
Cycrypto <jh01love@naver.com>
2021-07-14 12:41:24 +0900

    [test] 파라미터 슬라이싱 테스트

--- betaSandol/lambda_prototype.py
+++ betaSandol/lambda_prototype.py
@@ -37,8 +37,11 @@ def lambda_handler(event, context):
 
         elif key[0] == 'subway':
             gen = Generator.Return_Type()
-            setting_time = json.loads(request_body['action']['detailParams']['date_time']['value'])['time']
-            gen.is_Text(str(setting_time))
+            try:
+                setting_time = json.loads(request_body['action']['detailParams']['date_time']['value'])['time']
+                gen.is_Text(str(setting_time))
+            except Exception as e:
+                gen.is_Text(str(e))
 
             # return_string = "[4호선]\n" + Module.Test(time = setting_time).arrival_time() + "\n\n[수인분당선]\n"
             # return_string += Module.Test(time = setting_time, station_no="11120").arrival_time()
