fdceefb525987a4b14df1c3b8201fdd71add84ba
Cycrypto <jh01love@naver.com>
2021-07-14 13:04:13 +0900

    [enhancement] 지하철 블럭 업데이트

--- betaSandol/lambda_prototype.py
+++ betaSandol/lambda_prototype.py
@@ -38,14 +38,13 @@ def lambda_handler(event, context):
         elif key[0] == 'subway':
             gen = Generator.Return_Type()
             try:
-                # setting_time = json.loads(gen.is_Text(str(request_body['action']['detailParams']['date_time']['value'])))['time']
-                return_string = gen.is_Text(str(json.loads(request_body['action']['detailParams']['date_time']['value'])['time']))
+                setting_time = str(json.loads(request_body['action']['detailParams']['date_time']['value'])['time'])
+                return_string = "[4호선]\n" + Module.Test(time = setting_time).arrival_time() + "\n\n[수인분당선]\n"
+                return_string += Module.Test(time = setting_time, station_no="11120").arrival_time()
+                return_string = gen.is_Text(return_string)
+
             except Exception as e:
                 return_string = gen.is_Text(str(e))
-
-            # return_string = "[4호선]\n" + Module.Test(time = setting_time).arrival_time() + "\n\n[수인분당선]\n"
-            # return_string += Module.Test(time = setting_time, station_no="11120").arrival_time()
-            # return_string = gen.is_Text(return_string)
 
         elif key[0] == "ann":
             # return_string = Module.CrawlingFunction.subway(Module.CrawlingFunction)
