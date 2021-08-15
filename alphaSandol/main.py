279fbf1fa5c424af4e37216d06e1314b1209edd9
Cycrypto <jh01love@naver.com>
2021-08-15 12:40:00 +0900

    Module Update

--- alphaSandol/main.py
+++ alphaSandol/main.py
@@ -9,16 +9,36 @@ import base64
 import sandol_constant as Constant
 
 def lambda_handler(event, context):
+    return_string = ''
     request_body = event['body']
     request_body = json.loads(base64.b64decode(request_body))
     param = request_body['action']['params']
     key = list(param.keys())
-    function_params = list(param.values())
-    print("a")
-    if key[0] in Constant.KEY_SET.keys():
-        module_function = Constant.KEY_SET[key[0]]
-        print(module_function)
-    print(return_string)
+    func_name = key[0]
+    key_values = list(param.values())
+    ACCESS_ID = str(request_body['userRequest']['user']['properties']['botUserKey'])    #접근 권한을 가진 ID 확인용
+
+    module_function = Constant.KEY_SET[key[0]]  # 입력된 파라미터에 맞는 함수 ㅈ지정
+    input_params = Constant.PARAM_EXIST_FUNCTION[key[0]]
+
+    if len(input_params) == 0:
+        return_string = module_function()
+
+    elif len(input_params) == 1:
+            return_string = module_function(key_values[0])
+
+    else:
+        if func_name == 'read_feedback':
+            return_string = module_function(key_values[0], ACCESS_ID)
+
+        elif func_name == 'store_name':
+            upload_date = json.loads(key_values[3])
+            return_string = module_function(key_values[0], key_values[1], key_values[2], upload_date['date'], ACCESS_ID)
+
+        elif func_name == 'reset_meal':
+            upload_date = json.loads(key_values[3])
+            return_string = module_function(ACCESS_ID, upload_date)
+
 
     return {
         'statusCode': 200,
