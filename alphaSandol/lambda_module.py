10ac384d37c5d3cc64a756c4343653d46932ca3c
Gwang Jong Ko <rhkswhdwkd@naver.com>
2021-10-04 11:13:34 +0900

    Update lambda_module.py

--- alphaSandol/lambda_module.py
+++ alphaSandol/lambda_module.py
@@ -64,8 +64,7 @@ class AboutMeal:  # 학식 관련 클래스
 
                         ret += f"{form}[{str(last_update_date)} {weekday[last_update_date.weekday()]}요일]\n" \
                                f"{Constant.IMOGE['emotion']['paw']} 중식 : {menu_list[self.LUNCH]}\n" \
-                               f"{Constant.IMOGE['emotion']['paw']} 석식 : {menu_list[self.DINNER]}\n한식메뉴 포장가능"
-
+                               f"{Constant.IMOGE['emotion']['paw']} 석식 : {menu_list[self.DINNER]}\n한식메뉴 포장가능\n"
                     ret += "🐾웰스프레쉬 [URL 참조]\nhttps://ibook.kpu.ac.kr/Viewer/menu01"
 
                 return_string = MEAL_GEN.set_text(ret, is_init=False)  # 교외식당 저장
@@ -89,7 +88,7 @@ class AboutMeal:  # 학식 관련 클래스
                           f"{Constant.IMOGE['emotion']['paw']} 중식 : {menu_list[self.LUNCH]}\n" \
                           f"{Constant.IMOGE['emotion']['paw']} 석식 : {menu_list[self.DINNER]}"
                     if uid == "46f338132e6af63c32c07220c318f0e7c570e8eb6f375c9e8bb59ce33776f27c4c":
-                        ret += "한식메뉴 포장가능"
+                        ret += "한식메뉴 포장가능\n"
                     return_string = GEN.set_text(ret)
 
                 return return_string
