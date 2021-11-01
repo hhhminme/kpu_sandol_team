44099e84bd81addebca107c8d5b3290d528138ce
hhhminme <kenma409@gmail.com>
2021-11-01 19:25:49 +0900

    리턴형 변경

--- alphaSandol/lambda_module.py
+++ alphaSandol/lambda_module.py
@@ -238,7 +238,7 @@ class AboutMeal:  # 학식 관련 클래스
         title = "식당 계좌이체 결제"
         dsc = "식당 계좌이체 결제 디스크립션"
 
-        return GEN.set_card(Constant.SANDOL_LOGO1,btn_list,title,dsc,False);
+        return GEN.set_card(Constant.SANDOL_LOGO1,btn_list,title,dsc,True);
       
 
 class LastTraffic:  # 교통 관련 클래스
