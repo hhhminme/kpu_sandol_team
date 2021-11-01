2377703c8bc77a9b72fb8d4565d8b0dcb5b07800
hhhminme <kenma409@gmail.com>
2021-11-01 19:13:56 +0900

    리턴 형식 변경

--- alphaSandol/lambda_module.py
+++ alphaSandol/lambda_module.py
@@ -224,7 +224,7 @@ class AboutMeal:  # 학식 관련 클래스
 
     # 식당 계좌이체 결제
     def payment_meal(self):
-        btn_list = [{
+        btn_list = {
           "label" : "세미콘 식당",
           "action" : "webLink",
           "webLinkUrl" : "https://qr.kakaopay.com/2810060111751110120069009c404611"
@@ -233,13 +233,12 @@ class AboutMeal:  # 학식 관련 클래스
           "label" : "민이 식당",
           "action" : "webLink",
           "webLinkUrl" : "https://qr.kakaopay.com/2810060110000075262686359c406394"
-        }]
+        }
 
         title = "식당 계좌이체 결제"
         dsc = "식당 계좌이체 결제 디스크립션"
 
-        # return GEN.set_card(Constant.SANDOL_LOGO1,btn_list,title,dsc);
-        return GEN.set_text("hello")
+        return GEN.set_card(Constant.SANDOL_LOGO1,btn_list,title,dsc,False);
       
 
 class LastTraffic:  # 교통 관련 클래스
