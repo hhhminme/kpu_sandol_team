e51b3b58d986c29a04b3fa63ffb80139c3568f02
Cycrypto <jh01love@naver.com>
2021-11-03 20:53:11 +0900

    refactor main function

--- alphaSandol/main.py
+++ alphaSandol/main.py
@@ -1,73 +1,45 @@
+import json
+import base64
+import alphaSandol as constant
+
 if __name__ == '__main__':
     # main 파일을 엔트리포인트로 사용할 경우, path에 폴더 경로를 추가해 절대 경로로 임포트가 가능하게 함
     from sys import path
     import os
-    path.append(os.path.dirname(__file__))
 
-import json
-import base64
-import sandol_constant as Constant
-import random
+    path.append(os.path.dirname(__file__))
+    constant.DEBUG = True  # 디버깅모드 트리거
+
 
 def lambda_handler(event, context):
     try:
-        return_string = ''
-        request_body = event['body']
-        request_body = json.loads(base64.b64decode(request_body))
+        if constant.DEBUG:
+            func = "delete_feedback"  # 테스트할 함수
+            params = ["안뇽!"]  # 파라미터
+            access_id = 'd367f2ec55f41b4207156f4b8fce5ce885b05d8c3b238cf8861c55a9012f6f5895'  # 접근 ID
 
-        param = request_body['action']['params']
-        key = list(param.keys())
-        func_name = key[0]
-        key_values = list(param.values())
-        ACCESS_ID = str(request_body['userRequest']['user']['id'])  # 접근 권한을 가진 ID 확인용
-
-        module_function = Constant.KEY_SET[key[0]]  # 입력된 파라미터에 맞는 함수 지정
-        input_params = Constant.PARAM_EXIST_FUNCTION[key[0]]
-
-        if len(input_params) == 0:  # 파라미터가 없는 기능 함수의 경우 모두 여기서 처리 가능
-
-            if func_name == 'perm_chk':
-                from return_type_generator import return_type as GEN
-                return_string = GEN().set_text(ACCESS_ID)
-
-            else:
-                return_string = module_function()
-
-        elif len(input_params) == 1:    # 파라미터가 1개인 경우, but subway는 플러그인을 가지므로, 따로 처리
-            if func_name == 'subway':
-                time = str(json.loads(request_body['action']['detailParams']['date_time']['value'])['time'])
-                return_string = module_function(time)
-
-            elif func_name == 'read_meal':
-                return_string = module_function(ACCESS_ID)
-            else:
-                return_string = module_function(key_values[0])
+            return_json = function_handler(func, params, access_id)
 
         else:
-            if func_name == 'read_feedback':
-                if(str(key_values[0]) != '2'):
-                    return_string = module_function[0](ACCESS_ID)
-                else:
-                    return_string = module_function[1](ACCESS_ID)
+            return_json = ''
+            request_body = event['body']
+            request_body = json.loads(base64.b64decode(request_body))  # base64로 디코딩해야 제대로된 값을 받음
 
-            elif func_name == 'store_name':
-                upload_date = json.loads(key_values[3])
-                return_string = module_function(key_values[0], key_values[1], key_values[2], upload_date['date'],
-                                                ACCESS_ID)
-
-            elif func_name == 'reset_meal':
-                date = str(json.loads(param[key[0]])['date'])
-                return_string = module_function(ACCESS_ID, date)
-
+            param = request_body['action']['params']  # request json 접근용
+            key = list(param.keys())
+            func = key[0]  # 함수 호출시 사용할 값
+            key_values = list(param.values())  # 함수에 파라미터에 사용할 값
+            ACCESS_ID = str(request_body['userRequest']['user']['id'])  # 접근 권한을 가진 ID 확인용
+            return_json = function_handler(func, key_values, ACCESS_ID)
 
     except Exception as e:
-        return_string = {
+        return_json = {
             "version": "2.0",
             "template": {
                 "outputs": [
                     {
                         "simpleText": {
-                            "text": f"산돌이 기능 업데이트중입니다! 일부 기능이 사용이 제한됩니다{e}"
+                            "text": f"{constant.DEBUGGING('debug', 'main.lambda_handler', e)}"
                         }
                     }
                 ],
@@ -80,15 +52,46 @@ def lambda_handler(event, context):
                 ]
             }
         }
-    print(return_string)
+
+    return return_json
+
+
+def function_handler(func, key, access_id):
+    return_json = ''
+    if func == 'weather':   # 날씨 관련
+        import weather
+        return_json = weather.Weather().weather()
+
+    elif func == "covid":   # 코로나 관련
+        import covid
+        return_json = covid.Covid().today_covid()
+
+    elif func == "feedback_upload" or func == "read_feedback" or func == "delete_feedback":     # 피드백 관련
+        import feedback
+        feedback_class = feedback.Feedback()
+
+        if func == "feedback_upload":
+            return_json = feedback_class.upload_feedback(key[0])
+
+        elif func == "read_feedback":
+            return_json = feedback_class.read_feedback(access_id)
+
+        else:
+            return_json = feedback_class.delete_feedback(access_id)
+
+    elif func == "store_name" or func == "read_meal" or func == "reset_meal" or func == "time_meal" or "payment_meal":
+        import restaurant
+
     return {
         'statusCode': 200,
-        'body': json.dumps(return_string),
+        'body': json.dumps(return_json),
         'headers': {
             'Access-Control-Allow-Origin': '*',
         }
     }
 
-# if __name__ == "__main__":
-#     from test_constant import *
-#     print(lambda_handler(JSON_DATA, "asdf"))
+
+if __name__ == "__main__":      # Deploy 할때 무조건 주석처리 하기
+    func_call = lambda_handler("event", "context")
+    print(func_call)  # 디버깅용 함수 호출
+    print(json.loads(func_call['body']))  # json 유니코드 -> UTF 확인용
