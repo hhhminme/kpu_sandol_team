324b5a96b355f53c0c7ccf4c02809003f58ac019
Cycrypto <jh01love@naver.com>
2022-01-25 13:06:59 +0900

    fix: 코로나 블럭 api 업데이트로 인한 수정

--- alphaSandol/covid.py
+++ alphaSandol/covid.py
@@ -12,10 +12,10 @@ class Covid:
             url = 'https://m.search.naver.com/p/csearch/content/nqapirender.nhn?where=nexearch&pkid=9005&key=diffV2API'
             html = requests.get(url).text
             data = json.loads(html)
-            result = f"{data['result']['list'][-1]['date']}일까지 코로나 발생현황이에요 {settings.IMOGE('emotion', 'walk')}\n" \
-                     f"{settings.IMOGE('emotion', 'paw')}지역발생 : {data['result']['list'][-1]['local']}명\n" \
-                     f"{settings.IMOGE('emotion', 'paw')}해외발생 : {data['result']['list'][-1]['oversea']}명 입니다!\n " \
-                     f"코로나 조심하세요!{settings.IMOGE('emotion', 'nexpression')}"
+            result = f"{data['result']['baseDate'].split(' ')[0][:-1]}일까지 코로나 발생현황이에요 {settings.IMOGE('emotion', 'walk')}\n" \
+                     f"{settings.IMOGE('emotion', 'paw')}지역발생은 {data['result']['data']['dailyCnt'][-1]}명\n이며," \
+                     f"전일대비 {int(data['result']['data']['dailyCnt'][-1].replace(',','')) - int(data['result']['data']['dailyCnt'][-2].replace(',',''))}명 이에요" \
+                     f" 건강에 유의하세요!{settings.IMOGE('emotion', 'nexpression')}"
             # 결과 컨텍스트
 
             self.return_string = settings.GEN.set_card(settings.SANDOL_COVID_IMG, is_title="코로나 확진자 수",
