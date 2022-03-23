1adfb69276100a2e6009405c6282528d5ee6046d
Cycrypto <jh01love@naver.com>
2022-03-23 22:59:37 +0900

    Add : 산돌팀 22 ACCESS_ID 추가

--- alphaSandol/settings.py
+++ alphaSandol/settings.py
@@ -4,7 +4,6 @@ import json
 # 리턴 타입
 from return_type_generator import ReturnType, ParamOption
 
-
 GEN = ReturnType()  # kakao-i type json generator
 ParamOption = ParamOption
 
@@ -25,7 +24,12 @@ LOCAL_FEEDBACK_FILE: str = "/tmp/" + FEEDBACK_FILE  # 람다 서버 tmp 디렉�
 SANDOL_ACCESS_ID: dict = {'MANAGER': "d367f2ec55f41b4207156f4b8fce5ce885b05d8c3b238cf8861c55a9012f6f5895",
                           'CONT1': "339b0444bfabbffa0f13508ea7c45b61675b5720234cca8f73cd7421c22de9e546",
                           'CONT2': "04eabc8b965bf5ae6cccb122a18521969cc391162e3fd5f61b85efe8bb12e5e98a",
-                          'CONT3': "def99464e022b38389697fe68d54bbba723d1da291094c19bbf5eaace7b059a997"}
+                          'CONT3': "def99464e022b38389697fe68d54bbba723d1da291094c19bbf5eaace7b059a997",
+                          'MINJU': "1ddffbb02b29a6beb212d2f6fe2469523b9a90f260344b9d30677abcd977e1b56c",
+                          'YURIM': "16b3777b75026553a1807d1f361773e080e6441d30c65ea6e89dd0b84c9b58f071",
+                          'HAMIN': "c0657ad2b0ade045e546d8abb33f45d85f3c826ce797800e0bf25aac0652bf175c",
+                          'JDONG' : "cbdb6ec7c1427fd603a9c87ee5a1f7d1cc948ca896a2d65f88c770aa742218cef0"
+                          }
 # 산돌팀만 접근할 수 있는 컨텐츠에 인증 수단으로 사용 (현재 아이디의 정확한 위치가 기억이 나지 않아.. KEY를 메니저와, CONTRIBUTOR로 명명함.)
 
 RESTAURANT_ACCESS_ID: dict = {'미가식당': "32d8a05a91242ffb4c64b5630ec55953121dffd83a121d985e26e06e2c457197e6",
--- alphaSandol/weather.py
+++ alphaSandol/weather.py
@@ -1,52 +1,22 @@
 import settings
 from bs4 import BeautifulSoup
 import requests
+import datetime
 
 
 class Weather:
     def __init__(self):
-        self.URL = 'https://search.naver.com/search.naver?query='
-        self.return_string = ''
+        self.URL = r"https://weather.naver.com/today/02390132"
 
     def weather(self, location: str = "정왕") -> dict:
-        url = self.URL + (location + "날씨")
-        html = requests.get(url).text
-        soup = BeautifulSoup(html, 'html.parser')
-        weather_box = soup.find('section', {'class': 'sc_new cs_weather_new _cs_weather'})
-        info = weather_box.find('div', {'class': 'weather_graphic'})
-        weather_info = info.find("div", {"class": "weather_main"}).get_text()
-        temp = info.find("div", {"class": "temperature_text"}).get_text()
-        temp = temp[temp.find("도") + 1:]
-
-        temp_summary = weather_box.find("div", {"class": "temperature_info"})
-        compare_yesterday = temp_summary.find("p").get_text().split('  ')[0]
-
-        chart_list = weather_box.find("div", {"class": "report_card_wrap"})
-        chart = []  # 미세먼지, 초미세먼지, 자외선, 일몰 순서
-        for i in chart_list.find_all("li"):
-            chart.append(i.get_text().strip().split(" ")[1])
-
-        result = f"오늘 {location}의 날씨를 알려드릴게요!\n" \
-                  f"오늘 날씨는{settings.IMOGE('weather', weather_info.strip())}{weather_info}이고,\n" \
-                  f"기온은 {temp}C 으로 {compare_yesterday}\n"
-        try:
-            result += f"미세먼지는 {chart[0]}, \n"
-        except:
-            result += f"미세먼지는 -, \n"
-
-        try:
-            result += f"초미세먼지는 {chart[1]}이며, \n"
-        except:
-            result += f"초미세먼지는 -이며, \n"
-
-        try:
-            result += f"자외선은 {chart[2]} 입니다!"
-        except:
-            result += f"자외선은 - 입니다!"
-
+        soup = BeautifulSoup(requests.get(self.URL).text, 'html.parser')
+        weather_box = soup.select('#content > div > div.section_center > div.card.card_today > div.today_weather > div.weather_area')[0]
+        today = weather_box.find('p', {'class': 'summary'}).text.replace('\n', '  ').strip().split('  ')    # 날씨, 어제와 비교
+        today_date = weather_box.find('i', {'class': 'ico_animation _cnLazy'})['data-ymdt']
+        dates = datetime.datetime.strptime(today_date, '%Y%m%d%H')
+        result = f"{dates}기준 정왕 날씨입니다\n현재날씨는 {today[0]}이고, {today[1]}"
         return settings.GEN.set_text(result)
 
 
 if __name__ == "__main__":
-    import json
-    print(json.dumps(Weather().weather()))
+    print(Weather().weather())
